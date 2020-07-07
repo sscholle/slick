2b843c305b1a18ecbe0a3e335e8ca3495a581e49
Sebastian Scholle <sscholle@gmail.com>
2020-07-07 11:18:16 +0200

    Remove forced Jquery require

--- slick/slick.js
+++ slick/slick.js
@@ -15,17 +15,7 @@
 
  */
 /* global window, document, define, jQuery, setInterval, clearInterval */
-;(function(factory) {
-    'use strict';
-    if (typeof define === 'function' && define.amd) {
-        define(['jquery'], factory);
-    } else if (typeof exports !== 'undefined') {
-        module.exports = factory(require('jquery'));
-    } else {
-        factory(jQuery);
-    }
-
-}(function($) {
+(function($) {
     'use strict';
     var Slick = window.Slick || {};
 
@@ -3034,4 +3024,4 @@
         return _;
     };
 
-}));
+});
